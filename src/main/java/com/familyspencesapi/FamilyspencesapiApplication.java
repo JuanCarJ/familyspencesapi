cb5c41fcdb24c561c149dba290167a2be0a77a08
jpg27 <juanpablog01p@gmail.com>
2025-08-02 11:18:27 -0500

    Generacion de proyecto

---
+++ src/main/java/com/familyspencesapi/FamilyspencesapiApplication.java
@@ -0,0 +1,13 @@
+package com.familyspencesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyspencesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyspencesapiApplication.class, args);
+	}
+
+}
