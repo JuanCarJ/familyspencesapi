a0f65f9ab67db0ffa980bc9fce25407af854b8e1
xXDISSSELXx <jpabloquin@gmail.com>
2025-08-16 12:01:23 -0500

    Se creo el dominio de presupuesto mensual

---
+++ src/main/java/com/familyspencesapi/domain/presupuestoMensual/Presupuesto.java
@@ -0,0 +1,10 @@
+package com.familyspencesapi.domain.presupuestoMensual;
+
+import java.time.LocalDate;
+
+public class Presupuesto {
+
+    private LocalDate periodo;
+    private int presupuesto;
+
+}
