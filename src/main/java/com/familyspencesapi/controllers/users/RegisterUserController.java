95c59ba30e75f42e424531ebcb067225b8831e67
Brahian Rueda Gutierrez <brahianrueda123@gmail.com>
2025-09-05 15:31:20 -0500

    Se agrega el service RegisterUserService y se modifica el cotroller RegisterUserController para las validaciones necesarias para registrar un usuario

--- src/main/java/com/familyspencesapi/controllers/users/RegisterUserController.java
+++ src/main/java/com/familyspencesapi/controllers/users/RegisterUserController.java
@@ -1,48 +1,27 @@
 package com.familyspencesapi.controllers.users;
 
 import com.familyspencesapi.domain.users.RegisterUser;
+import com.familyspencesapi.services.users.RegisterUserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-        import java.time.LocalDate;
-import java.util.*;
 
 @RestController
 @RequestMapping("/api/users/register")
 public class RegisterUserController {
 
-    private final List<RegisterUser> users = new ArrayList<>();
+    private final RegisterUserService registerUserService;
+
+    public RegisterUserController(RegisterUserService registerUserService) {
+        this.registerUserService = registerUserService;
+    }
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody RegisterUser user) {
-        if (user == null ||
-                user.getfullName() == null || user.getfullName().isEmpty() ||
-                user.getbirthDate() == null ||
-                user.getdocumentType() == null ||
-                user.getdocument() == null || user.getdocument().isEmpty() ||
-                user.getEmail() == null || user.getEmail().isEmpty() ||
-                user.getRelationship() == null ||
-                user.getcredit_card() == null || user.getcredit_card().isEmpty() ||
-                user.getphone() == null || user.getphone().isEmpty() ||
-                user.getAddress() == null || user.getAddress().isEmpty() ||
-                user.getPassword() == null || user.getPassword().isEmpty()) {
-            return ResponseEntity.badRequest().body("Todos los campos son obligatorios.");
+        try {
+            RegisterUser createdUser = registerUserService.createUser(user);
+            return ResponseEntity.ok(createdUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        user.setId(UUID.randomUUID());
-        user.setfamilyId(UUID.randomUUID());
-        users.add(user);
-        return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/{id}")
-    public RegisterUser getUser(@PathVariable UUID id) {
-        return users.stream()
-                .filter(u -> u.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
-
-    @GetMapping
-    public List<RegisterUser> getAllUsers() {
-        return users;
     }
 }
